819ff6fcb4d95f0cab649b1291dba127e8ef49eb
franckfotso1 <73475604+franckfotso1@users.noreply.github.com>
2021-07-20 17:29:18 +0200

    feat(live-status): data-in-table

--- src/client/South/SouthStatus.jsx
+++ src/client/South/SouthStatus.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Label, List, Row, Breadcrumb, BreadcrumbItem, Container } from 'reactstrap'
 import { AlertContext } from '../context/AlertContext.jsx'
+import Table from '../components/table/Table.jsx'
 
 const SouthStatus = () => {
   // const [messages, setMessages] = React.useState([])
@@ -27,6 +28,51 @@ const SouthStatus = () => {
     }
     return (() => source.close())
   }, [])
+
+  // start modification for data in table ,,,,,,,,,,,
+
+  /**
+   * Generate string value from on object.
+   * @param {Object[]} data - The object
+   * @return {string} - The string value
+   */
+  const generateStringValueFromObject = (data) => {
+    let stringValue = ''
+    Object.entries(data).forEach(([key, value]) => {
+      if (key !== 'name') {
+        stringValue += stringValue ? ` / ${key}: ${value}` : `${key}: ${value}`
+      }
+    })
+    return stringValue
+  }
+
+  /**
+   * Generate row entry for the status table.
+   * @param {string} key - The key
+   * @param {string} value - The value
+   * @return {[{name: *, value: *}, {name: string, value: *}]} - The table row
+   */
+  const generateRowEntry = (key, value) => [
+    {
+      name: key,
+      value: key,
+    },
+    {
+      name: 'value',
+      value,
+    },
+  ]
+
+  const tableRows = []
+  Object.keys(connectorData).forEach((key) => {
+    if (Array.isArray(connectorData[key])) {
+      connectorData[key].forEach((entry) => {
+        tableRows.push(generateRowEntry(entry.name, generateStringValueFromObject(entry)))
+      })
+    } else {
+      tableRows.push(generateRowEntry(key, connectorData[key]))
+    }
+  })
 
   return (
     <>
@@ -63,6 +109,9 @@ const SouthStatus = () => {
           ))}
         </List>
       </Container>
+      <Row>
+        <Container>{tableRows && <Table headers={[]} rows={tableRows} />}</Container>
+      </Row>
     </>
   )
 }
